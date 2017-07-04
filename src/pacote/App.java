b1947c77ab8962250c3ce67b8caccbef75d4bd80
anderson <anderson.civil@hotmail.com>
2017-07-03 23:17:00 -0300

    ajustes

---
+++ src/pacote/App.java
@@ -0,0 +1,12 @@
+package pacote;
+/*
+ * run the app
+ */
+import java.io.IOException;
+
+public class App {
+	public static void main(String[] args) throws IOException {
+		Reader_v1 arq = new Reader_v1();
+		arq.leSimulacao();
+	}
+}
